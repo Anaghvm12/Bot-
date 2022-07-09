219a4a5cb9aa2cbc638270c5891f21aa9bc824c9
DARK-WEBLOAD <108183565+DARKWEBLOAD-TG@users.noreply.github.com>
2022-07-09 20:50:29 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -32,6 +32,7 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     CORONA_TXT ="""<b>Here is the help for the coron information module</b>
   /covid <code>(countryname)</code> <b>you can find a corona information of every country</b>"""
+    STICKER_TXT ="""<b>COMMAND /stickerid\n𝖨𝖿 𝖸𝗈𝗎 𝖭𝖾𝖾𝖽 𝖳𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 𝖢𝗅𝗂𝖼𝗄 /stickerid 𝖳𝗈 𝖦𝖾𝗍 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 (𝖱𝖾𝗉𝗅𝗒 𝖶𝗂𝗍𝗁 𝖲𝗍𝗂𝖼𝗄𝖾𝗋)</b>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
 - Eva Maria Supports both url and alert inline buttons.
