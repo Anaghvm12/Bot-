979bd7c2d9a4e300af43761fbb0da13ee8339321
DARK-WEBLOAD <108183565+DARKWEBLOAD-TG@users.noreply.github.com>
2022-07-09 22:17:03 +0530

    Update ping.py

--- plugins/Dwl/ping.py
+++ plugins/Dwl/ping.py
@@ -31,3 +31,8 @@ async def ping(_, message):
 @Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
 async def repo(_, message):
     await message.reply_text(REPO)
+
+
+@Client.on_message(filters.command("owner", COMMAND_HAND_LER) & f_onw_fliter)
+async def check_alive(_, message):
+    await message.reply_text(OWNER)
